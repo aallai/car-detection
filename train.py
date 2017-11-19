42e8d6f99829e75a92b1817c884310bb55f02023
Alexandre Allaire <aallaire@Alexandres-iMac.fios-router.home>
2017-11-19 17:05:51 -0500

    Add classifer and HOG visualization.

--- train.py
+++ train.py
@@ -2,9 +2,11 @@ import argparse
 import numpy as np
 import cv2
 import glob
-from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
+from sklearn.model_selection import train_test_split
 
 IMAGE_SIZE = (64, 64)
 
@@ -14,52 +16,7 @@ HOG_CELLS_PER_BLOCK = (2, 2)
 HOG_ANGLE_BINS = 12
 
 # Color histogram parameters.
-COLOR_BINS = 8
-
-
-def load_dataset(positive_dirs, negative_dirs):
-    positives = []
-    negatives = []
-
-    for d in positive_dirs:
-        for file in glob.glob(d + "/*.png"):
-            positives.append(cv2.imread(file))
-
-    for d in negative_dirs:
-        for file in glob.glob(d + "/*.png"):
-            positives.append(cv2.imread(file))
-
-    print(np.array(positives).shape)
-    print(np.array(negatives).shape)
-
-    X = np.vstack((positives, negatives))
-    y = np.vstack((np.ones(len(positives)), np.zeros(len(negatives))))
-    return X, y
-
-#
-# Get HOG and LUV histogram features from images.
-# This function should not change the order of X.
-#
-def extract_features(X, colorspace, visualize=False):
-    features = []
-    for img in X:
-        img = cv2.cvtColor(img, colorspace)
-
-        hist_features = []
-        hog_features = []
-        hog_images = []
-        for i in range(3):
-            features, hog_image = hog(img, orientations=HOG_ANGLE_BINS, pixels_per_cell=HOG_CELL_SIZE, cells_per_block=HOG_CELLS_PER_BLOCK, block_norm='L2-Hys', visualize=False, transform_sqrt=False, feature_vector=True)
-            hog_features.append(features)
-            hog_images.append(hog_image)
-            hist_features.append(np.histogram(img, COLOR_BINS))
-
-        print(np.float64(hist_features).ravel().shape)
-        print(np.float64(hog_features).ravel().shape)
-
-        features.append(np.concatenate((np.float64(hist_features).ravel(), np.float64(hog_features).ravel())))
-
-    return np.array(features)
+COLOR_BINS = 32
 
 def get_colorspace(str):
     if str == 'RGB':
@@ -75,20 +32,98 @@ def get_colorspace(str):
     else:
         raise Exception('Invalid colorspace provided: ' + str)
 
+def load_dataset(positive_dirs, negative_dirs):
+    positives = []
+    negatives = []
+
+    for d in positive_dirs:
+        for file in glob.glob(d + "/*.png"):
+            positives.append(cv2.imread(file))
+
+    for d in negative_dirs:
+        for file in glob.glob(d + "/*.png"):
+            negatives.append(cv2.imread(file))
+
+    images = np.vstack((positives, negatives))
+    labels = np.hstack((np.ones(len(positives)), np.zeros(len(negatives))))
+    return images, labels
+
+def visualize_hog(original, hog_image, colorspace):
+    fig = plt.figure()
+    plt.subplot(1, 4, 1)
+    plt.imshow(original, cmap='gray')
+    plt.title('HOG features for colorspace {}.'.format(colorspace))
+
+    for i in range(3):
+        plt.subplot(1, 4, 2 + i)
+        plt.imshow(hog_image[i], cmap='gray')
+
+    plt.savefig('./output_images/hog.png')
+
+#
+# Get HOG and LUV histogram features from images.
+# This function should not change the order of X.
+#
+def extract_features(X, colorspace, visualize=False):
+
+    data = []
+    images = []
+    for img in X:
+        img = cv2.cvtColor(img, get_colorspace(colorspace))
+
+        hist_features = []
+        hog_features = []
+        hog_images = []
+        for i in range(3):
+            features, hog_image = hog(img[...,i], orientations=HOG_ANGLE_BINS, pixels_per_cell=HOG_CELL_SIZE, cells_per_block=HOG_CELLS_PER_BLOCK, block_norm='L2-Hys', visualise=True, transform_sqrt=False, feature_vector=True)
+            hog_features.append(features)
+            hog_images.append(hog_image)
+            hist_features.append(np.histogram(img, COLOR_BINS)[0])
+
+        data.append(np.concatenate((np.array(hist_features).ravel(), np.array(hog_features).ravel())))
+        images.append(hog_images)
+
+    if (visualize):
+        visualize_hog(X[0], images[0], colorspace)
+
+    return np.array(data)
+
+def train(X, y):
+    scaler = StandardScaler().fit(X)
+    normed_X = scaler.transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(normed_X, y, test_size=0.2)
+
+    parameters = {'kernel':('linear', 'rbf', 'poly')}
+    algo = svm.SVC()
+    classifier = grid_search.GridSearchCV(svr, parameters)
+    classifier.fit(X_train, y_train)
+
+    print("Best classifier parameters:")
+    print(classifier.best_params_)
+    print("Best classifier accuracy:")
+    print(classifier.score(X_test, y_test))
+
+    return classifier
+
 def main():
     parser = argparse.ArgumentParser(description='Train classifier')
     parser.add_argument('-p', '--posisitve-dirs', nargs='+', default=[], help='Directory(s) containing positive (vehicle) training images.')
     parser.add_argument('-n', '--negative-dirs', nargs='+', default=[], help='Directory(s) containing negative (non-vehicle) training images.')
     parser.add_argument('-c', '--colorspace', default='LUV', help='Colorspace to use for features. Can be RGB, LUV, HLS, YUV, or YCrCb.')
+    parser.add_argument('-v', '--visualize', action="store_true", help='Visualize hog features.')
     args = parser.parse_args()
 
     if (not args.posisitve_dirs or not args.negative_dirs):
         raise Exception('You must provide both positive and negative training data.')
 
-    X, y = load_dataset(args.posisitve_dirs, args.negative_dirs)
-    X = extract_features(X, get_colorspace(args.colorspace))
+    images, y = load_dataset(args.posisitve_dirs, args.negative_dirs)
+    X = extract_features(images, args.colorspace, args.visualize)
 
+    print("Dataset dimensions:")
     print(X.shape)
+
+    classifier = train(X, y)
 
 if __name__ == '__main__':
     main()
